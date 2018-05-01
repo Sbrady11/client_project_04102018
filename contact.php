d0082a4c12e9b6186ffc1bd85b7fecbcd3e8a18f
Simon Brady <simonbrady@mac.com>
2018-05-01 17:36:55 -0400

    style
    change buttons, update email, update font

--- contact.php
+++ contact.php
@@ -3,13 +3,13 @@ if( isset($_POST['n']) && isset($_POST['e']) && isset($_POST['m']) ){
 	$n = $_POST['n']; // HINT: use preg_replace() to filter the data
 	$e = $_POST['e'];
 	$m = nl2br($_POST['m']);
-	$to = "simonbrady@mac.com";	
+	$to = "inquiries@itamionline.com";	
 	$from = $e;
-	$subject = 'Contact Form Message';
+	$subject = 'Inquiry';
 	$message = '<b>Name:</b> '.$n.' <br><b>Email:</b> '.$e.' <p>'.$m.'</p>';
 	$headers = "From: $from\n";
 	$headers .= "MIME-Version: 1.0\n";
-	$headers .= "Content-type: text/html; charset=iso-8859-1\n";
+	$headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
 	if( mail($to, $subject, $message, $headers) ){
 		echo "success";
 	} else {
